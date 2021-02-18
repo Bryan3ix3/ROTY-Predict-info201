2323b0ea55db480266dbd5d848c889bda0339ab9
Bryan Ahaneku <btac3ix3@uw.edu>
2021-02-17 16:12:00 -0800

    Complete chart 2 V1

---
+++ chart_2.R
@@ -0,0 +1,19 @@
+library(styler)
+library(tidyverse)
+library(ggplot2)
+library(reshape2)
+
+# reads historical data of ROTY's
+historic_stats <- read.csv("https://raw.githubusercontent.com/info201a-w21/project-luffyzor/main/Data/ROTY%20Historic%20Stats.csv?token=ASMBEZPX6V65RW3JY3YHSHTAGDJ3C")
+
+# making dataframe of important stats
+df <- historic_stats[, c("YEAR", "PPG", "AST", "REB")]
+head(df)
+
+# melting data so it can be graphed
+df_melt <- reshape2::melt(df, id.var = "YEAR")
+
+# line graph
+ggplot(df_melt, aes(x = YEAR, y = value, colour = variable)) +
+  geom_line() + xlab("Year") + ylab("Points") + labs(color = "Stats")
+

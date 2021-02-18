5a175f81c54600176dff6dc9ea384f5c5bf0ac85
luffyzor <luffyzor@uw.edu>
2021-02-18 07:34:56 -0800

    added line chart to var

--- scripts/chart_2.R
+++ scripts/chart_2.R
@@ -15,7 +15,7 @@ head(df)
 df_melt <- reshape2::melt(df, id.var = "YEAR")
 
 # line graph
-ggplot(df_melt, aes(x = YEAR, y = value, colour = variable)) +
+line_chart <- ggplot(df_melt, aes(x = YEAR, y = value, colour = variable)) +
   geom_line(size = 1) + xlab("Year") + ylab("Points") + labs(color = "Stats") +
   ggtitle("Historic ROTY stats") +
   theme(plot.title = element_text(hjust = 0.5))
